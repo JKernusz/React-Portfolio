4789853cd5330b8fe6361d478408f226940d7efd
John Kay <jkernusz1346@gmail.com>
2023-12-02 19:49:38 -0500

    finished all links

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,13 +4,17 @@ import {Link} from 'react-router-dom'
 function Project({projects}) {
   return (
     <div>
-    <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src={projects.image} alt="Shoes" /></figure>
+    <div className="card w-96 bg-base-100 shadow-xl mx-2">
+      <Link to={projects.github} >
+        <figure><img src={projects.image} alt="Shoes"   /></figure>
+      </Link>
   <div className="card-body">
     <h2 className="card-title">{projects.title}</h2>
     <p>{projects.description}</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <Link to={projects.link}>
+        <button className="btn btn-primary">Link</button>
+      </Link>
     </div>
   </div>
 </div>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import Project from '../components/Project'
-import tripperImg from '../assets/picofme.png'
+import tripperImg from '../assets/tripper.png'
+import whadayaImg from '../assets/whadaya.jpeg'
+import artbeatsImg from '../assets/artbeats.png'
+import weathernowImg from '../assets/weathernow.png'
+import workdayImg from '../assets/workdayscheduler.png'
+import codingqwizImg from '../assets/codingqwiz.png'
+
 function Portfolio() {
   const projects = [
     {
@@ -14,7 +20,7 @@ function Portfolio() {
     {
       id: 2,
       title: 'Whadaya',
-      image: tripperImg,
+      image: whadayaImg,
       description: 'Social media-type platform',
       link: 'https://whadaya-production.up.railway.app/',
       github: 'https://github.com/provardjake/Whadaya'
@@ -22,7 +28,7 @@ function Portfolio() {
     {
       id: 3,
       title: 'ArtBeats',
-      image: tripperImg,
+      image: artbeatsImg,
       description: 'Artistic media-type platform',
       link: 'https://jeremyblau.github.io/ArtBeats/',
       github: 'https://github.com/JeremyBlau/ArtBeats'
@@ -30,7 +36,7 @@ function Portfolio() {
     {
       id: 4,
       title: 'Coding Qwiz',
-      image: tripperImg,
+      image: codingqwizImg,
       description: 'Early dev. Quiz application',
       link: 'https://jkernusz.github.io/Coding-qWiz/',
       github: 'https://github.com/JKernusz/Coding-qWiz'
@@ -38,7 +44,7 @@ function Portfolio() {
     {
       id: 5,
       title: 'Work Scheduler',
-      image: tripperImg,
+      image: workdayImg,
       description: 'Calendar application',
       link: 'https://jkernusz.github.io/Work-Scheduler/',
       github: 'https://github.com/JKernusz/Work-Scheduler'
@@ -46,7 +52,7 @@ function Portfolio() {
     {
       id: 6,
       title: 'WeatherNow',
-      image: tripperImg,
+      image: weathernowImg,
       description: 'Early dev. weather application',
       link: 'https://jkernusz.github.io/WeatherNow/',
       github: 'https://github.com/JKernusz/WeatherNow'
