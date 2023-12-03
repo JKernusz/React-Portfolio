1f1b7d2bffc185adb597b21839ea749656480b2f
John Kay <jkernusz1346@gmail.com>
2023-12-02 20:31:27 -0500

    fixed links for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
     <div>
       <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
         <nav className="grid grid-flow-col gap-4">
-          <Link className="link link-hover">About me</Link>
-          <Link className="link link-hover">Contact</Link>
-          <Link className="link link-hover">resume</Link>
-          <Link className="link link-hover">Portfolio</Link>
+          <Link className="link link-hover" to="/about">About me</Link>
+          <Link className="link link-hover" to="/contact">Contact</Link>
+          <Link className="link link-hover" to="/resume">Resume</Link>
+          <Link className="link link-hover" to="/portfolio">Portfolio</Link>
         </nav> 
         <nav>
           <div className="grid grid-flow-col gap-4">
